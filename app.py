4044146276524f9095a82a13dce6872b80f12bca
Kevin Krummsdorf <70586330+ITKev95@users.noreply.github.com>
2024-06-14 17:00:24 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,45 @@
+from openai import OpenAI
 import streamlit as st
 
-st.title('Chatbot v1.0')
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+def click_one():
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state.messages.append({"role": "assistant", "content": "Switched to GPT-3.5-Turbo"})
+
+def click_two():
+    st.session_state["openai_model"] = "gpt-4"
+    st.session_state.messages.append({"role": "assistant", "content": "Switched to GPT-4"})
+
+def click_three():
+    st.session_state.messages.append({"role": "assistant", "content":st.session_state["openai_model"]})
+    
+st.title("Your own GPT chatbot!")
+st.write("This is a simple chatbot that uses OpenAI's GPT to generate responses")
+st.write("You can switch between different models by clicking the buttons below")
+st.write("If you click on “current version” the currently selected model will be displayed")
+st.write("The preselected model is GPT-3.5-Turbo")
+
+col1, col2, col3= st.columns([1,1,1])
+
+with col1:
+    st.button('1 gpt-3.5-turbo', on_click=click_one)
+with col2:
+    st.button('2 gpt-4', on_click=click_two)
+with col3:
+    st.button('3 current version', on_click=click_three)
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
